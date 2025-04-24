66c86530c18b601f59cf68d623bb86c36a209840
Sergio Enrique Rojas Medina <69541817+seper7304@users.noreply.github.com>
2025-04-24 15:11:55 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Bienvenido a BITMART SPOT BOT</h1>
+      <p>Tu simulador de bots de trading con datos en tiempo real.</p>
+    </div>
+  )
+}
+
+export default App
